a90d8cbb83b0f53cb46e478695dd8f7d5cea1ff2
TikhovaMariya <tmg1999@yandex.ru>
2018-02-23 17:12:22 +0300

    + BinarySearch
    Hoare Triples added

--- java/search/BinarySearch.java
+++ java/search/BinarySearch.java
@@ -5,9 +5,13 @@ import java.lang.String;
 import java.lang.IndexOutOfBoundsException;
 
 public class BinarySearch {
+    //Pre: forall i < j: a[i] >= a[j]
     private static int binSearch(int x, int[] a) {
         int l = -1, r = a.length;
         int m;
+        //I: l >= l' && r <= r' && l < r
+        //   forall 0 <= i <= l: a[i] > x
+        //   forall r <= j < a.length:  a[i] <= x
         while (l < r - 1) {
             m = (l + r) / 2;
             if (a[m] > x) {
@@ -18,17 +22,30 @@ public class BinarySearch {
         }
         return r;
     }
+    //Post: x = x'
+    //      a.length = a'.length
+    //      a[i] = a'[i] for i = 0...a.length - 1
+    //      r <= r' == a.length && r > l' == -1
+    //      R: r such that (r < a.length && a[r] <= x && a[r - 1] > x) ||
+    //                     (r == a.length && (a.length < 1 || a[i] > x for i = 1...a.length))
 
-    private static int recursiveBinSearch(int x, int[] a, int left, int right) {
-        int middle = (left + right) / 2;
-        if(right - left <= 1) {
-            return right;
+    //Pre: forall i < j: a[i] >= a[j]
+    private static int recursiveBinSearch(int x, int[] a, int l, int r) {
+        if (r - l <= 1) {
+            return r;
         }
-        if (a[middle] > x) {
-            return recursiveBinSearch(x, a, middle, right);
+        int m = (l + r) / 2;
+        if (a[m] > x) {
+            return recursiveBinSearch(x, a, m, r);
         }
-        return recursiveBinSearch(x, a, left, middle);
+        return recursiveBinSearch(x, a, l, m);
     }
+    //Post: x = x'
+    //      a.length = a'.length
+    //      a[i] = a'[i] for i = 0...a.length - 1
+    //      r <= r' == a.length && r > l' == -1
+    //      R: r such that (r < a.length && a[r] <= x && a[r - 1] > x) ||
+    //                     (r == a.length && (a.length < 1 || a[i] > x for i = 1'...r'))
 
     public static void main(String[] args) {
         try {
